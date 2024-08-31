c24636b9736915a114f7159ff8754b7da5ea7ea4
RuiTakao <ruia1082halfnc@gmail.com>
2024-08-31 21:24:45 +0900

    日付ソート実装

--- app/src/main/java/com/example/kakeibo_dev_6/component/page/expenditureDetail/parts/SearchDialog.kt
+++ app/src/main/java/com/example/kakeibo_dev_6/component/page/expenditureDetail/parts/SearchDialog.kt
@@ -41,6 +41,8 @@ import com.example.kakeibo_dev_6.MainViewModel
 @Composable
 fun SearchDialog(isShowSearchDialog: MutableState<Boolean>, viewModel: MainViewModel) {
     if (isShowSearchDialog.value) {
+        val sort = remember { mutableStateOf(viewModel.sort) }
+
         AlertDialog(
             onDismissRequest = { isShowSearchDialog.value = false },
             modifier = Modifier
@@ -51,103 +53,150 @@ fun SearchDialog(isShowSearchDialog: MutableState<Boolean>, viewModel: MainViewM
                 Column(
                     modifier = Modifier.padding(top = 24.dp)
                 ) {
-                    var value = ""
-                    Text(
-                        text = "表示期間",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
-                    TextField(
-                        value = value,
-                        onValueChange = {
-                            value = it
-                        },
-                        modifier = Modifier
-                            .width(250.dp)
-                            .background(Color.White)
-                    )
+                    DateDurationField()
                     Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = "カテゴリ",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
-                    Box(
-                        contentAlignment = Alignment.CenterStart,
-                        modifier = Modifier
-                            .size(250.dp ,50.dp)
-                            .height(50.dp)
-                            .clip(RoundedCornerShape(4.dp))
-                            .border(
-                                BorderStroke(1.dp, Color.LightGray),
-                                RoundedCornerShape(4.dp)
-                            )
-                    ) {
-                        Text(text = "すべて", modifier = Modifier.padding(start = 10.dp))
-                        Icon(
-                            imageVector = Icons.Filled.ArrowDropDown,
-                            contentDescription = "選択アイコン",
-                            modifier = Modifier.align(Alignment.CenterEnd)
-
-                        )
-                    }
+                    SelectCategoryBox()
                     Spacer(modifier = Modifier.height(16.dp))
-
-                    val expanded = remember { mutableStateOf(false) }
-                    Text(
-                        text = "表示順",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
-                    Box(
-                        contentAlignment = Alignment.CenterStart,
-                        modifier = Modifier
-                            .size(250.dp ,50.dp)
-                            .clip(RoundedCornerShape(4.dp))
-                            .border(
-                                BorderStroke(1.dp, Color.LightGray),
-                                RoundedCornerShape(4.dp)
-                            )
-                            .clickable { expanded.value = !expanded.value }
-                    ) {
-                        Text(
-                            text = "日付${if (viewModel.sort) "昇順" else "降順"}",
-                            modifier = Modifier.padding(start = 10.dp)
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.ArrowDropDown,
-                            contentDescription = "選択アイコン",
-                            modifier = Modifier.align(Alignment.CenterEnd)
-
-                        )
-                        DropdownMenu(
-                            expanded = expanded.value,
-                            onDismissRequest = { expanded.value = false },
-                            modifier = Modifier.width(250.dp)
-                        ) {
-                            DropdownMenuItem(
-                                text = { Text(text = "日付降順") },
-                                onClick = { /*TODO*/ }
-                            )
-                            DropdownMenuItem(text = { Text(text = "日付昇順") },
-                                onClick = { /*TODO*/ }
-                            )
-                        }
-                    }
-
+                    SelectDateSortBox(value = sort, viewModel = viewModel)
                 }
                 Row(
                     horizontalArrangement = Arrangement.End,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    TextButton(onClick = { /*TODO*/ }) {
+                    TextButton(onClick = { isShowSearchDialog.value = false }) {
                         Text(text = "キャンセル")
                     }
-                    TextButton(onClick = { /*TODO*/ }) {
+                    TextButton(onClick = {
+                        isShowSearchDialog.value = false
+                        viewModel.sort = sort.value
+                    }) {
                         Text(text = "OK")
                     }
                 }
             }
+        }
+    }
+}
+
+// 表示期間のテキストボックス
+@Composable
+private fun DateDurationField() {
+    var value = ""
+    Text(
+        text = "表示期間",
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(bottom = 4.dp)
+    )
+    TextField(
+        value = value,
+        onValueChange = {
+            value = it
+        },
+        modifier = Modifier
+            .width(250.dp)
+            .background(Color.White)
+    )
+}
+
+// カテゴリのセレクトボックス
+@Composable
+private fun SelectCategoryBox() {
+    val expanded = remember { mutableStateOf(false) }
+    Text(
+        text = "カテゴリ",
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(bottom = 4.dp)
+    )
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = Modifier
+            .size(250.dp, 50.dp)
+            .height(50.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .border(
+                BorderStroke(1.dp, Color.LightGray),
+                RoundedCornerShape(4.dp)
+            )
+            .clickable { expanded.value = !expanded.value }
+    ) {
+        Text(text = "すべて", modifier = Modifier.padding(start = 10.dp))
+        Icon(
+            imageVector = Icons.Filled.ArrowDropDown,
+            contentDescription = "選択アイコン",
+            modifier = Modifier.align(Alignment.CenterEnd)
+        )
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+            modifier = Modifier.width(250.dp)
+        ) {
+            DropdownMenuItem(
+                text = { Text(text = "日付降順") },
+                onClick = {
+                    expanded.value = false
+//                    value.value = false
+                }
+            )
+            DropdownMenuItem(
+                text = { Text(text = "日付昇順") },
+                onClick = {
+                    expanded.value = false
+//                    value.value = true
+                }
+            )
+        }
+    }
+}
+
+// 表示順のセレクトボックス
+@Composable
+private fun SelectDateSortBox(value: MutableState<Boolean>, viewModel: MainViewModel) {
+    val expanded = remember { mutableStateOf(false) }
+    Text(
+        text = "表示順",
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(bottom = 4.dp)
+    )
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = Modifier
+            .size(250.dp, 50.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .border(
+                BorderStroke(1.dp, Color.LightGray),
+                RoundedCornerShape(4.dp)
+            )
+            .clickable { expanded.value = !expanded.value }
+    ) {
+        Text(
+            text = "日付${if (value.value) "昇順" else "降順"}",
+            modifier = Modifier.padding(start = 10.dp)
+        )
+        Icon(
+            imageVector = Icons.Filled.ArrowDropDown,
+            contentDescription = "選択アイコン",
+            modifier = Modifier.align(Alignment.CenterEnd)
+
+        )
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+            modifier = Modifier.width(250.dp)
+        ) {
+            DropdownMenuItem(
+                text = { Text(text = "日付降順") },
+                onClick = {
+                    expanded.value = false
+                    value.value = false
+                }
+            )
+            DropdownMenuItem(
+                text = { Text(text = "日付昇順") },
+                onClick = {
+                    expanded.value = false
+                    value.value = true
+                }
+            )
         }
     }
 }
