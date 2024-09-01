dd321d4654cda10450530d22b9b68a260a446d24
RuiTakao <ruia1082halfnc@gmail.com>
2024-09-01 22:19:25 +0900

    支出項目登録、レイアウト修正

--- app/src/main/java/com/example/kakeibo_dev_6/component/page/editExpenditureItem/EditExpendItem.kt
+++ app/src/main/java/com/example/kakeibo_dev_6/component/page/editExpenditureItem/EditExpendItem.kt
@@ -1,6 +1,8 @@
 package com.example.kakeibo_dev_6.component.page.editExpenditureItem
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,9 +11,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.DatePicker
@@ -36,8 +41,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,6 +54,7 @@ import androidx.navigation.NavController
 import com.example.kakeibo_dev_6.MainViewModel
 import java.time.Instant
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -119,22 +129,28 @@ fun EditExpenditureItem(
                 .padding(top = 32.dp)
                 .padding(horizontal = 16.dp)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "日付", modifier = Modifier.width(80.dp))
+            Column {
 
+                // 日付
+                Text(text = "日付", modifier = Modifier.padding(bottom = 4.dp), fontWeight = FontWeight.Bold)
                 // DatePicker Start
                 Box(
                     modifier = Modifier
-                        .background(Color.LightGray)
-                        .padding(start = 16.dp)
-                        .height(48.dp)
-                        .fillMaxWidth()
+                        .size(260.dp, 50.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                        .border(
+                            BorderStroke(1.dp, Color.LightGray),
+                            RoundedCornerShape(4.dp)
+                        )
                         .clickable { visible = !visible },
                     contentAlignment = Alignment.Center
                 ) {
                     Text(text = payDate)
+                    Icon(
+                        imageVector = Icons.Filled.ArrowDropDown,
+                        contentDescription = "選択アイコン",
+                        modifier = Modifier.align(Alignment.CenterEnd)
+                    )
                     if (visible) {
                         DatePickerDialog(onDismissRequest = { visible = false }, confirmButton = {
                             TextButton(onClick = { visible = false }) {
@@ -160,10 +176,10 @@ fun EditExpenditureItem(
 
             }
             Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "金額", modifier = Modifier.width(80.dp))
+
+            // 金額
+            Column {
+                Text(text = "金額", modifier = Modifier.padding(bottom = 4.dp), fontWeight = FontWeight.Bold)
                 TextField(
                     value = price,
                     onValueChange = {
@@ -172,28 +188,37 @@ fun EditExpenditureItem(
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     modifier = Modifier
                         .background(Color.White)
-                        .padding(start = 16.dp)
-                        .fillMaxWidth()
-                        .height(48.dp)
+                        .width(260.dp)
                 )
             }
             Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "カテゴリ", modifier = Modifier.width(80.dp))
+
+            // カテゴリー
+            Column {
+                Text(text = "カテゴリー", modifier = Modifier.padding(bottom = 4.dp), fontWeight = FontWeight.Bold)
                 Box(
+                    contentAlignment = Alignment.CenterStart,
                     modifier = Modifier
-                        .background(Color.LightGray)
-                        .padding(start = 16.dp)
-                        .height(48.dp)
-                        .clickable { expanded.value = !expanded.value },
-                    contentAlignment = Alignment.Center
+                        .size(260.dp, 50.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                        .border(
+                            BorderStroke(1.dp, Color.LightGray),
+                            RoundedCornerShape(4.dp)
+                        )
+                        .clickable { expanded.value = !expanded.value }
                 ) {
-                    Text(text = selectOptionText.value)
+                    Text(text = selectOptionText.value, modifier = Modifier.padding(start = 10.dp))
+                    Icon(
+                        imageVector = Icons.Filled.ArrowDropDown,
+                        contentDescription = "選択アイコン",
+                        modifier = Modifier.align(Alignment.CenterEnd)
+                    )
+
                     DropdownMenu(
                         expanded = expanded.value,
-                        onDismissRequest = { expanded.value = false }) {
+                        modifier = Modifier.width(260.dp),
+                        onDismissRequest = { expanded.value = false }
+                    ) {
                         options.forEach { selectOption ->
                             DropdownMenuItem(text = { Text(text = selectOption.name) }, onClick = {
                                 selectOptionText.value = selectOption.name
@@ -203,13 +228,11 @@ fun EditExpenditureItem(
                         }
                     }
                 }
-
             }
+
             Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "内容", modifier = Modifier.width(80.dp))
+            Column {
+                Text(text = "内容", modifier = Modifier.padding(bottom = 4.dp), fontWeight = FontWeight.Bold)
                 TextField(
                     value = content,
                     onValueChange = {
@@ -217,7 +240,7 @@ fun EditExpenditureItem(
                     },
                     modifier = Modifier
                         .background(Color.White)
-                        .fillMaxWidth()
+                        .width(260.dp)
                 )
             }
         }
