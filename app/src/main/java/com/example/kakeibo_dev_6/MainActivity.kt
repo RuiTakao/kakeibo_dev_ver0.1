b33f09bd966d260086f52d43a9f816f47dc5176a
RuiTakao <ruia1082halfnc@gmail.com>
2024-09-06 18:48:30 +0900

    デザイン修正

--- app/src/main/java/com/example/kakeibo_dev_6/MainActivity.kt
+++ app/src/main/java/com/example/kakeibo_dev_6/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.kakeibo_dev_6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.core.app.AppLaunchChecker
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.kakeibo_dev_6.component.Navigation
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,17 +13,47 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            setUp()
+            if (!AppLaunchChecker.hasStartedFromLauncher(applicationContext)) {
+                // 初回起動の場合実行
+                val viewModel: MainViewModel = hiltViewModel()
+                defaultCategory(viewModel)
+            }
+
+            // アプリの起動ステータス監視
+            AppLaunchChecker.onActivityCreate(this)
+
             Navigation()
         }
     }
 }
 
-@Composable
-fun setUp(viewModel: MainViewModel = hiltViewModel()) {
-    viewModel.content = "食費"
-    viewModel.createExpendItem()
-    viewModel.name = "食費"
+private fun defaultCategory(viewModel: MainViewModel) {
+
+    viewModel.name = "🏠 生活費"
     viewModel.order = 1
+    viewModel.createCategory()
+
+    viewModel.name = "🛍 日用品費"
+    viewModel.order = 2
+    viewModel.createCategory()
+
+    viewModel.name = "🎾 娯楽費"
+    viewModel.order = 3
+    viewModel.createCategory()
+
+    viewModel.name = "🍙 食費"
+    viewModel.order = 4
+    viewModel.createCategory()
+
+    viewModel.name = "👪 交際費"
+    viewModel.order = 5
+    viewModel.createCategory()
+
+    viewModel.name = "📺 家具・家電"
+    viewModel.order = 6
+    viewModel.createCategory()
+
+    viewModel.name = "❓ その他"
+    viewModel.order = 7
     viewModel.createCategory()
 }
